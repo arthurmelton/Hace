625a57850576c53e59b284a255fe77fd6830f08e
arthurmelton <amtitan@icloud.com>
2022-02-22 14:53:13 -0500

    pidof

--- src/memory.rs
+++ src/memory.rs
@@ -1,3 +1,14 @@
-pub fn hello() {
-    println!("Hello");
+use sysinfo::{System, SystemExt, ProcessExt, PidExt};
+pub fn pidof(x:&str) -> u32 {
+    let mut system = System::new_all();
+    system.refresh_all();
+    let x = match sysinfo::SystemExt::processes_by_name(&system,x).nth(0) {
+        Some(x) => {
+            x.pid().as_u32()
+        },
+        None => {
+            0
+        }
+    };
+    return x;
 }
